823d6ef4e7e13911a3c3f5e6240e31d452f1a8eb
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-10-30 16:52:36 +0900

    check3

---
+++ chap1/check3.rs
@@ -0,0 +1,12 @@
+fn main() {
+    for i in 1..51 {
+        if i % 3 == 0 || i % 10 == 3 {
+            println!("A");
+        }
+        if i >= 30 && i <= 39 {
+            println!("A");
+            continue
+        }
+        println!("{}", i)
+    }
+}
